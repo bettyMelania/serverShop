302740171a95e2e8fc157c19a9a22a3f05a97087
bettyMelania <melania_betty_95@yahoo.com>
2017-12-19 13:10:45 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,83 @@
+import Koa from 'koa';
+import cors from 'koa-cors';
+import convert from 'koa-convert';
+import bodyParser from 'koa-bodyparser';
+import Router from 'koa-router';
+import http from 'http';
+import socketIo from 'socket.io';
+import dataStore from 'nedb-promise';
+import {getLogger, timingLogger, errorHandler, jwtConfig} from './utils';
+import {ProductRouter} from './product-router';
+import {AuthRouter} from './auth-router';
+import koaJwt from 'koa-jwt';
+import socketioJwt from 'socketio-jwt';
+
+const app = new Koa();
+const router = new Router();
+const server = http.createServer(app.callback());
+const io = socketIo(server);
+const log = getLogger('app');
+
+app.use(timingLogger);
+app.use(errorHandler);
+
+app.use(bodyParser());
+app.use(convert(cors()));
+
+const apiUrl = '/api';
+
+log('config public routes');
+const authApi = new Router({prefix: apiUrl})
+const userStore = dataStore({filename: '../users.json', autoload: true});
+authApi.use('/auth', new AuthRouter({userStore, io}).routes())
+app.use(authApi.routes()).use(authApi.allowedMethods())
+
+log('config protected routes');
+app.use(convert(koaJwt(jwtConfig)));
+const protectedApi = new Router({prefix: apiUrl})
+const productStore = dataStore({filename: '../products.json', autoload: true});
+protectedApi.use('/product', new ProductRouter({productStore, io}).routes())
+app.use(protectedApi.routes()).use(protectedApi.allowedMethods());
+
+io.on('connection', socketioJwt.authorize(jwtConfig))
+  .on('authenticated', (socket) => {
+    const username = socket.decoded_token.username;
+    socket.join(username);
+    log(`${username} authenticated and joined`);
+    socket.on('disconnect', () => {
+      log('${username} disconnected');
+    })
+  });
+
+(async() => {
+  log('ensure default data');
+  const ensureUserAndProducts = async(username) => {
+    let user = await userStore.findOne({username: username});
+    if (user) {
+      log(`user ${username} was in the store`);
+    } else {
+      user = await userStore.insert({username, password: username});
+      log(`user added ${JSON.stringify(user)}`);
+    }
+    let products = await productStore.find({user: user._id});
+    if (products.length > 0) {
+      log(`user ${username} had ${products.length} products`);
+    } else {
+      for (let i = 0; i < 3; i++) {
+        let product = await productStore.insert({
+          id:`${i}`,
+          name: `Products ${username}${i}`,
+          amount:2*`${i}`,
+          price: 3*`${i}`,
+          updated: Date.now(),
+          user: user._id,
+          version: 1
+        });
+        log(`product added ${JSON.stringify(product)}`);
+      }
+    }
+  };
+  await Promise.all(['betty', 'bea'].map(username => ensureUserAndProducts(username)));
+})();
+
+server.listen(3000);
---
+++ src/product-router.js
@@ -0,0 +1,133 @@
+import {
+  OK, NOT_FOUND, LAST_MODIFIED, NOT_MODIFIED, BAD_REQUEST, ETAG,
+  CONFLICT, METHOD_NOT_ALLOWED, NO_CONTENT, CREATED, FORBIDDEN, setIssueRes
+} from './utils';
+import Router from 'koa-router';
+import {getLogger} from './utils';
+
+const log = getLogger('product');
+
+let productsLastUpdateMillis = null;
+
+export class ProductRouter extends Router {
+  constructor(props) {
+    super(props);
+    this.productStore = props.productStore;
+    this.io = props.io;
+    this.get('/', async(ctx) => {
+      let res = ctx.response;
+      let lastModified = ctx.request.get(LAST_MODIFIED);
+      if (lastModified && productsLastUpdateMillis && productsLastUpdateMillis <= new Date(lastModified).getTime()) {
+        log('search / - 304 Not Modified (the client can use the cached data)');
+        res.status = NOT_MODIFIED;
+      } else {
+        res.body = await this.productStore.find({user: ctx.state.user._id});
+        if (!productsLastUpdateMillis) {
+            productsLastUpdateMillis = Date.now();
+        }
+        res.set({[LAST_MODIFIED]: new Date(productsLastUpdateMillis)});
+        log('search / - 200 Ok');
+      }
+    }).get('/:id', async(ctx) => {
+      let product = await this.productStore.findOne({_id: ctx.params.id});
+      let res = ctx.response;
+      if (product) {
+        if (product.user == ctx.state.user._id) {
+          log('read /:id - 200 Ok');
+          this.setNoteRes(res, OK, product); //200 Ok
+        } else {
+          log('read /:id - 403 Forbidden');
+          setIssueRes(res, FORBIDDEN, [{error: "It's not your product"}]);
+        }
+      } else {
+        log('read /:id - 404 Not Found (if you know the resource was deleted, then you can return 410 Gone)');
+        setIssueRes(res, NOT_FOUND, [{warning: 'Note not found'}]);
+      }
+    }).post('/', async(ctx) => {
+      let product = ctx.request.body;
+      let res = ctx.response;
+      if (product.name) { //validation
+          product.user = ctx.state.user._id;
+        await this.createNote(ctx, res, product);
+      } else {
+        log(`create / - 400 Bad Request`);
+        setIssueRes(res, BAD_REQUEST, [{error: 'Text is missing'}]);
+      }
+    }).put('/:id', async(ctx) => {
+      let product = ctx.request.body;
+      let id = ctx.params.id;
+      let productId = product._id;
+      let res = ctx.response;
+      if (productId && productId != id) {
+        log(`update /:id - 400 Bad Request (param id and body _id should be the same)`);
+        setIssueRes(res, BAD_REQUEST, [{error: 'Param id and body _id should be the same'}]);
+        return;
+      }
+      if (!product.name) {
+        log(`update /:id - 400 Bad Request (validation errors)`);
+        setIssueRes(res, BAD_REQUEST, [{error: 'Text is missing'}]);
+        return;
+      }
+      if (!productId) {
+        await this.createNote(ctx, res, product);
+      } else {
+        let persistedProduct = await this.productStore.findOne({_id: id});
+        if (persistedProduct) {
+          if (persistedProduct.user != ctx.state.user._id) {
+            log('update /:id - 403 Forbidden');
+            setIssueRes(res, FORBIDDEN, [{error: "It's not your product"}]);
+            return;
+          }
+          let productVersion = parseInt(ctx.request.get(ETAG)) || product.version;
+          if (!productVersion) {
+            log(`update /:id - 400 Bad Request (no version specified)`);
+            setIssueRes(res, BAD_REQUEST, [{error: 'No version specified'}]); //400 Bad Request
+          } else if (productVersion < persistedProduct.version) {
+            log(`update /:id - 409 Conflict`);
+            setIssueRes(res, CONFLICT, [{error: 'Version conflict'}]); //409 Conflict
+          } else {
+              product.version = productVersion + 1;
+              product.updated = Date.now();
+            let updatedCount = await this.productStore.update({_id: id}, product);
+              productsLastUpdateMillis = product.updated;
+            if (updatedCount == 1) {
+              this.setNoteRes(res, OK, product); //200 Ok
+              this.io.to(ctx.state.user.username).emit('product/updated', product);
+            } else {
+              log(`update /:id - 405 Method Not Allowed (resource no longer exists)`);
+              setIssueRes(res, METHOD_NOT_ALLOWED, [{error: 'Note no longer exists'}]); //
+            }
+          }
+        } else {
+          log(`update /:id - 405 Method Not Allowed (resource no longer exists)`);
+          setIssueRes(res, METHOD_NOT_ALLOWED, [{error: 'Note no longer exists'}]); //Method Not Allowed
+        }
+      }
+    }).del('/:id', async(ctx) => {
+      let id = ctx.params.id;
+      await this.productStore.remove({_id: id, user: ctx.state.user._id});
+      this.io.to(ctx.state.user.username).emit('product/deleted', {_id: id})
+        productsLastUpdateMillis = Date.now();
+      ctx.response.status = NO_CONTENT;
+      log(`remove /:id - 204 No content (even if the resource was already deleted), or 200 Ok`);
+    });
+  }
+
+  async createNote(ctx, res, product) {
+      product.version = 1;
+      product.updated = Date.now();
+    let insertedNote = await this.productStore.insert(product);
+      productsLastUpdateMillis = product.updated;
+    this.setNoteRes(res, CREATED, insertedNote); //201 Created
+    this.io.to(ctx.state.user.username).emit('product/created', insertedNote);
+  }
+
+  setNoteRes(res, status, product) {
+    res.body = product;
+    res.set({
+      [ETAG]: product.version,
+      [LAST_MODIFIED]: new Date(product.updated)
+    });
+    res.status = status; //200 Ok or 201 Created
+  }
+}
